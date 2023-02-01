fc38b38b1afad420bff2bae6d3abfed6c20a7d64
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-02-01 14:23:06 -0300

    Model postagem criada

---
+++ src/main/java/com/blogPessoal/model/Postagem.java
@@ -0,0 +1,54 @@
+package com.blogPessoal.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "titulo nao pode ser varios nada")
+    @Size(min = 5,max = 100, message = "Digite um titulo entre 5 e 100 caracteres")
+    private String titulo;
+    @NotBlank(message = "titulo nao pode ser varios nada")
+    @Size(min = 10,max = 1000, message = "Digite um texto entre 10 e 1000 caracteres")
+    private String texto;
+    private LocalDateTime data;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+}
