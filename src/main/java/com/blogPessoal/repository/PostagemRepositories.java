0c6c38db7a7616aa8e7868547c4ca2841846f7de
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-02-02 14:27:27 -0300

    Adição dos param

--- src/main/java/com/blogPessoal/repository/PostagemRepositories.java
+++ src/main/java/com/blogPessoal/repository/PostagemRepositories.java
@@ -2,6 +2,7 @@ package com.blogPessoal.repository;
 
 import com.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,13 +11,14 @@ import java.util.Optional;
 @Repository
 public interface PostagemRepositories extends JpaRepository<Postagem,Long> {
 
-    public List<Postagem> findByTituloContainingIgnoreCase (String titulo);
+    public List<Postagem> findByTituloContainingIgnoreCase (@Param("titulo") String titulo);
+
+    public List<Postagem> findByTextoContainingIgnoreCase (@Param("texto") String texto);
+
 
     public Optional<Postagem> findById(Long id);
 
-    public List<Postagem> saveAll();
+
 
 
 }
-
-
