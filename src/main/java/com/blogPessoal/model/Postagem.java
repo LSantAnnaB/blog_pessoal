31b93cbed435ae2ba74a1a7b912225f8b4922286
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-02-15 06:58:37 -0300

    Tema Repository criada

--- src/main/java/com/blogPessoal/model/Postagem.java
+++ src/main/java/com/blogPessoal/model/Postagem.java
@@ -1,5 +1,7 @@
 package com.blogPessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,13 +13,41 @@ public class Postagem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    @JoinColumn(name = "tema_id")
+    private Tema tema;
+
     @NotBlank(message = "titulo nao pode ser varios nada")
     @Size(min = 5,max = 100, message = "Digite um titulo entre 5 e 100 caracteres")
     private String titulo;
+
     @NotBlank(message = "titulo nao pode ser varios nada")
     @Size(min = 10,max = 1000, message = "Digite um texto entre 10 e 1000 caracteres")
     private String texto;
+
     private LocalDateTime data;
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
 
 
     public Long getId() {
